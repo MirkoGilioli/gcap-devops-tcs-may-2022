36f14ba04b0d2dc45aeb8c60bb9d77afcb7998c4
Mirkogilioli <mirko.gilioli@gmail.com>
2022-05-31 13:27:45 +0200

    typo in the test_help.py has been fixed

--- tests/functional/test_help.py
+++ tests/functional/test_help.py
@@ -8,7 +8,7 @@ def test_help_page(client):
     assert resp.status_code == 200
     assert b'Welcome this is the help Page' in resp.data
 
-def test_home_methodNotAllowed(client):
+def test_help_methodNotAllowed(client):
     '''
     GIVEN a Flask application
     WHEN  the '/help' page is hit with POST method
