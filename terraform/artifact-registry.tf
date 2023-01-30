c1219da187348e04299d9e2190084460bbf19885
Mirko Gilioli <mirko.gilioli@gmail.com>
2023-01-30 11:25:06 +0100

    typo has been correct in the artifact registry

--- terraform/artifact-registry.tf
+++ terraform/artifact-registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "webapp-repo" {
   location      = "us-central1"
-  repository_id = "webapp-repo
+  repository_id = "webapp-repo"
   description   = "This is the repository that will contains the images of the webapp"
   format        = "DOCKER"
 }
