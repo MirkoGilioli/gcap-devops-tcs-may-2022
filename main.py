334755f247dda5700826c11956193090d759da41
Mirko Gilioli <mirko.gilioli@gmail.com>
2023-02-17 14:38:27 +0100

    added cloud debugger to the project

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from app import flask_app
-
+import googleclouddebugger
 if __name__ == '__main__':
+    try:
+        googleclouddebugger.enable(breakpoint_enable_canary=False)
+    except ImportError:
+        pass
+
     flask_app.run(debug=True, host='0.0.0.0', port=8080)
